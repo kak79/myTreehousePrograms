3573c84da52806d060bb4995c655a166ac5345c3
Kim <kohelka@gmail.com>
2020-06-05 09:20:29 -0500

    Add files via upload

--- Random Number Generator.js
+++ Random Number Generator.js
@@ -1,3 +1,3 @@
 let maxNumber = prompt("Please enter any number of 2 or more");
-let randomNumber = Math.floor( Math.random() * maxNumber ) + 1;
+let randomNumber = Math.floor( Math.random() * +maxNumber ) + 1;
 alert(`The random number is ${randomNumber}.`)
