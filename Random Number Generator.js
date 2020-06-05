60b87af2a1c46642f8be36f52d15eb95ba8ccfe3
Kim <kohelka@gmail.com>
2020-06-05 10:15:51 -0500

    Add files via upload

--- Random Number Generator.js
+++ Random Number Generator.js
@@ -1,3 +1,20 @@
-let maxNumber = prompt("Please enter any number of 2 or more");
-let randomNumber = Math.floor( Math.random() * +maxNumber ) + 1;
-alert(`The random number is ${randomNumber}.`)
+let maxNumber = prompt("Please enter your high number");
+maxNumber = parseInt(maxNumber);
+
+let lowNumber = prompt("Please enter your low number");
+lowNumber = parseInt(lowNumber);
+
+if (  maxNumber && lowNumber ){
+                    
+    let randomNumber =  Math.floor( Math.random() * (maxNumber - lowNumber + 1) ) + lowNumber;
+    alert(`The random number is ${randomNumber} between ${lowNumber} and ${maxNumber}`)
+
+}
+
+
+
+else {
+
+    alert("Please try again.")
+
+}
